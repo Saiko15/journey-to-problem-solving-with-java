d648e0d6b0331fa6b9b348b7623176a10d313a3d
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-11 12:37:32 +0200

    adding welcome file

---
+++ WelcomeProject/WelcomeProject/WelcomeProject.java
@@ -0,0 +1,11 @@
+package WelcomeProject;
+
+
+public class WelcomeProject {
+    public static void main(String[] args) {
+        
+        System.out.print("welcome");
+        System.out.print("ahmed");
+
+    }
+}
