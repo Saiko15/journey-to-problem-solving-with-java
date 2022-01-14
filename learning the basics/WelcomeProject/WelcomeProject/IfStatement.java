103b7280c48f0c7be6b1f9f7bb426f4b2c5ad493
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-14 07:09:54 +0200

    adding IfStatment file 0.0.3 "Adding

--- learning the basics/WelcomeProject/WelcomeProject/IfStatement.java
+++ learning the basics/WelcomeProject/WelcomeProject/IfStatement.java
@@ -41,9 +41,16 @@ public class IfStatement {
         // ###############
 
         if (a >= 90 && a <= 100) // multi way statement , Anding & Oring
-            System.out.println("A")
-        else if
-            System.out.println("A")
+            System.out.println("A");
+        else if (a >= 80 && a <= 90)
+            System.out.println("B");
+        else if (a >= 70 && a <= 80)
+            System.out.println("c");
+        else if (a >= 60 && a <= 70)
+            System.out.println("D");
+        else 
+            System.out.println("F");
+
     }
     
 }
