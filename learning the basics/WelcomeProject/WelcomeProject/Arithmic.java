a8651730576daf9a78d623e25e6b3d2db6b8defc
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-14 04:37:08 +0200

    adding arithmetic file 0.0.1

---
+++ learning the basics/WelcomeProject/WelcomeProject/Arithmic.java
@@ -0,0 +1,11 @@
+package WelcomeProject;
+
+public class Arithmic {
+    public static void main(String[] args) {
+        System.out.println(5 / 2);
+        System.out.println(5.0 / 2);
+        System.out.println( (float) 5 / 2);
+        System.out.println(5 / (float) 2);
+
+    }
+}
