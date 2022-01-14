6d681f7e1e524b07e87f9550fb583adf4d873879
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-14 04:52:18 +0200

    adding arithmetic file 0.0.2

--- learning the basics/WelcomeProject/WelcomeProject/Arithmic.java
+++ learning the basics/WelcomeProject/WelcomeProject/Arithmic.java
@@ -2,10 +2,20 @@ package WelcomeProject;
 
 public class Arithmic {
     public static void main(String[] args) {
-        System.out.println(5 / 2);
-        System.out.println(5.0 / 2);
-        System.out.println( (float) 5 / 2);
-        System.out.println(5 / (float) 2);
+        System.out.println(5 / 2); // 2
+        System.out.println(5.0 / 2); // 2.5
+        System.out.println( (float) 5 / 2); // 2.5 
+        System.out.println(5 / (float) 2); // 2.5
+
+        // ++i = pre-increment
+        // i++ = post-increment
+
+        // --i = pre-decrement
+        // i-- = post-decrement
+        int i = 9;
+        System.out.println(i++); // 9
+        System.out.println(i); // 10 
+        System.out.println(++i); // 11
 
     }
 }
