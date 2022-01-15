d84af1fa1141bc2cd7d6da7473c1f8301152aaa7
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-15 20:55:26 +0200

    Practicing Switch

---
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Switch.java
@@ -0,0 +1,47 @@
+package WelcomeProject;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter your choice:");
+        System.out.println("1. Add two numbers");
+        System.out.println("2. Substract two numbers");
+        System.out.println("3. Get the square of the number");
+
+
+
+        /*
+        char grade;
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter your grade");
+        grade = reader.next().charAt(0);
+
+        switch (grade)
+        {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Very good");
+                break;
+            case 'C':
+                System.out.println("Good");
+                break;
+            case 'D':
+                System.out.println("Acceptable");
+                break;
+            case 'F':
+                System.out.println("Failed");
+                break;
+            default :
+                System.out.println("Invalid Grade");
+             
+        }   
+        */
+        
+    }
+    
+}
