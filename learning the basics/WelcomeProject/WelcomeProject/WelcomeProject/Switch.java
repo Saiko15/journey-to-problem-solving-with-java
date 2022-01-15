4f054910588acda14a996ce44f8506b132701626
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-15 21:07:38 +0200

    Practicing Switch 1.0.1

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Switch.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Switch.java
@@ -4,11 +4,25 @@ import java.util.Scanner;
 
 public class Switch {
     public static void main(String[] args) {
+
+        int choice, num1, num2;
+        Scanner reader = new Scanner(System.in);
         
         System.out.println("Enter your choice:");
         System.out.println("1. Add two numbers");
         System.out.println("2. Substract two numbers");
         System.out.println("3. Get the square of the number");
+        choice = reader.nextInt();
+
+        switch (choice)
+        {
+            case 1:
+            System.out.println("1. Enter two number");
+            num1 = reader.nextInt();
+            num2 = reader.nextInt();
+            System.out.println(num1 + num2);
+            break;
+        }
 
 
 
