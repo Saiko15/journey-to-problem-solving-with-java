a1aa5e00f70643582f10d33d605fcf69ca2599ee
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-15 23:11:52 +0200

    Practicing While Loop 1.0.2

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/WhileLoop.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/WhileLoop.java
@@ -7,6 +7,21 @@ public class WhileLoop {
         int grade=0, counter=1, sum=0;
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Enter grades or -1 to exit");
+        while (counter<=6 && grade!= -1)
+        {
+            grade = reader.nextInt();
+            sum += grade;
+            counter ++;
+        }
+        if ( sum != -1)
+        System.out.println("Sum is : " + sum);
+        else System.out.println("Closed");
+      
+        /*
+        int grade=0, counter=1, sum=0;
+        Scanner reader = new Scanner(System.in);
+
         while (counter<=6)
         {
         System.out.println("Enter grade of studend no" + counter + ":");
@@ -16,7 +31,7 @@ public class WhileLoop {
 
         }
         System.out.println("the average is:" + sum/6);
-        
+        */
     }
     
 }
