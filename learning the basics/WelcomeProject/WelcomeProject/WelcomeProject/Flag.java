4887a5909822a676e8d7fc65add0a15ef7f77f6d
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-17 10:26:29 +0200

    Created a game with Flag statement

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Flag.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Flag.java
@@ -4,7 +4,33 @@ import java.util.Scanner;
 
 public class Flag {
     public static void main(String[] args) {
-        double sum=0.0, value;
+        
+      int guess, rand;
+      rand = (int)(Math.random()*100); // int because random generates douples, *100 because it'll from 0 to 1
+      System.out.println("the random number is: "+ rand);
+    
+      boolean stillplaying = true;
+      Scanner input= new Scanner(System.in);
+
+      while (stillplaying== true)
+      {
+          System.out.println("Guess a number:");
+          guess = input.nextInt();
+
+          if (guess > rand)
+            System.out.println("Guess is too large");
+          else if (guess < rand)
+            System.out.println("Guess is too small");
+          else 
+          {
+            System.out.println("You win!");
+            stillplaying = false;
+          }
+      }
+
+      
+      
+        /* double sum=0.0, value;
         boolean positive = true;
         Scanner input = new Scanner(System.in);
 
@@ -19,6 +45,7 @@ public class Flag {
                 positive = false;
         }
         System.out.println("the sum is : " + sum);
+        */
     }
     
 }
