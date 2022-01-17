21bc76f5dae7ff04487a96b765b607b09be19648
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-17 12:47:04 +0200

    getting the factorial with For loop

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/ForLoop.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/ForLoop.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class ForLoop {
     public static void main(String[] args) {
 
+        int number, factorial=1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number that you want its factorial");
+
+        number = input.nextInt();
+        for (int i=1; i<=number;i++)
+        {
+            factorial *= i;
+        }
+        System.out.println("Factorial is : " + factorial);
+
+
+
+
+        /*
         int sum=0,grade,counter;
         Scanner input = new Scanner(System.in);
 
@@ -16,6 +31,21 @@ public class ForLoop {
 
         }
         System.out.println("Sum is: " + sum/6);
+*/
+        /*
+            for (int i=0; i<=10; i++)
+                System.out.println(i);
+            ------------------
+            for (int i=0; i<=10; i+=2) // Even
+                System.out.println(i);
+            ------------------
+            for (int i=1; i<=10; i+=2) // Odd
+                System.out.println(i);
+            ------------------
+            for (int i=10; i>=0; i--) // decrement
+                System.out.println(i);
+            ------------------
+        */
         
     }
     
