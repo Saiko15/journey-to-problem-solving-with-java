acc1f7d09c1261c6db6eb121e9c698fa18139824
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-17 21:34:06 +0200

    sum function

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Functions.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Functions.java
@@ -5,6 +5,14 @@ public class Functions {
         drawline('*',10); // char should be '' not "" because "" is string
         drawline('#',20);
         drawline('~',50);
+
+        int total = sum(50, 100);
+        System.out.println(total);     
+        System.out.println(sum(60, 20));
+        System.out.println(sum(10, 20)*5/3); // press tab to jump to next parameter
+
+
+
     }
     public static void drawline(char ch, int length){
         for(int i=1;i<=length;i++)
@@ -13,6 +21,12 @@ public class Functions {
             // System.out.println();
         }
         System.out.println();
+    }
+    public static int sum(int i ,int j)
+    {
+        int result = 0;
+        result = i + j;
+        return result; // int function must have return keyword
     }
     
 }
