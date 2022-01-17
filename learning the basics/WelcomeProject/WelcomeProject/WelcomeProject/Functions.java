442921113633c44f71465955488cd52ebbc76046
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-17 21:24:19 +0200

    playing with draw line

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Functions.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Functions.java
@@ -2,12 +2,12 @@ package WelcomeProject;
 
 public class Functions {
     public static void main(String[] args) {
-        drawline('*'); // char should be '' not "" because "" is string
-        drawline('#');
-        drawline('~');
+        drawline('*',10); // char should be '' not "" because "" is string
+        drawline('#',20);
+        drawline('~',50);
     }
-    public static void drawline(char ch){
-        for(int i=1;i<=10;i++)
+    public static void drawline(char ch, int length){
+        for(int i=1;i<=length;i++)
         {
             System.out.print(ch);
             // System.out.println();
