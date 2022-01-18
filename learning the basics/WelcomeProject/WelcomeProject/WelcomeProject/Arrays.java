e0bb6140e7d82eb2fcaea7eb8ba8463c2736a733
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-18 11:13:08 +0200

    practicing array basics 'creating salaries calculator'

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Arrays.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/Arrays.java
@@ -5,19 +5,36 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String[] args) {
         int size;
+        double sum=0;
         Scanner input= new Scanner(System.in);
 
         System.out.println("Enter arrat size:");
         size= input.nextInt();
         
-        double[] list = new double[size]; 
-        char[] ch = new char[size];
-        boolean[] bo = new boolean[size];
+        double[] list = new double[size];
+
+        for(int i=0;i<list.length;i++)
+        {
+            System.out.println("Enter the salary of employee no "+ (i+1));
+            list[i]= input.nextDouble();
+            sum += list[i];
+
+        }
+        System.out.println("The sum is: "+ sum);
+        System.out.println("The average is: "+ sum/list.length);
+        
 
 
-        System.out.println(list[5]); // 0.0
-        System.out.println(ch[6]); // ''
-        System.out.println(bo[7]); // false
+
+
+        // double[] list = new double[size]; 
+        // char[] ch = new char[size];
+        // boolean[] bo = new boolean[size];
+
+
+        // System.out.println(list[5]); // 0.0
+        // System.out.println(ch[6]); // ''
+        // System.out.println(bo[7]); // false
 
     }
     
