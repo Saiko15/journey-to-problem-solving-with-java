1c1f0dec81fc4f2a34d599c06c0f5d3262bada37
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-21 18:17:40 +0200

    add projects folder

--- learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/TwoDArray.java
+++ learning the basics/WelcomeProject/WelcomeProject/WelcomeProject/TwoDArray.java
@@ -15,6 +15,27 @@ public class TwoDArray {
         for (int col=0 ; col<marks[1].length ; col++)
         System.out.println(marks[1][col]);
 
+        /*
+        void printMatrix( int matrix[ ][4], int Rows) 
+
+            {
+
+            int row, col;
+
+            for (row = 0; row < Rows ; row++)
+
+            {
+
+                for (col = 0; col < 4; col++)
+
+                System.out.println( setw(5) << matrix [row]
+                [col] ;
+
+            }
+
+            System.out.println(“\n” ;
+        */
+
 
         // int[][] arr= {{2, 1, 3},
         //               {3, 4, 5},
