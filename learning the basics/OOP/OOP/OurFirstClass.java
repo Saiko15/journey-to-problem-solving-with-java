dc49ab0f807f464b89f302d29083b68275690819
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-24 10:48:15 +0200

    First OOP Project 0.0.3

--- learning the basics/OOP/OOP/OurFirstClass.java
+++ learning the basics/OOP/OOP/OurFirstClass.java
@@ -4,6 +4,9 @@ public class OurFirstClass {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
         r1.width = 20.6; // because width is public
-
+        // r1.length = 10.3; // cannot accessed because it's private
+        r1.setlength(3);
+        r1.setwidth(6);
+        System.out.println(r1.width); //  6.0
     }
 }
