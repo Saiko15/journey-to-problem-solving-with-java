597582165abd2b3c1295ed6489e9fd25a2a9271b
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-24 10:38:04 +0200

    First OOP Project

---
+++ learning the basics/OOP/OOP/OurFirstClass.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class OurFirstClass {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle();
+        r1.width = 20.6; // because width is public
+
+    }
+}
