2425931f10f56a45ae75b263290f418556230c08
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-24 12:51:41 +0200

    Create Employee class

---
+++ learning the basics/OOP/OOP/Employee.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class Employee {
+    int emp_id;
+    String emp_name;
+    double salary;
+    double bonus;
+    boolean residant;
+
+    public void print_emp ()
+    {
+        System.out.println(emp_id);
+        System.out.println(emp_name);
+        System.out.println(salary);
+        System.out.println(bonus);
+        System.out.println(residant);
+    }
+}
+
