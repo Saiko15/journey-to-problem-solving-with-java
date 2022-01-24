279df6d391c53193a7fec87a8557c8cb9f1c61ea
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-24 10:46:08 +0200

    First OOP Project 0.0.2

---
+++ learning the basics/OOP/OOP/Rectangle.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class Rectangle {
+    private double length;
+    public double width;
+
+    public void setlength (double l)
+    {
+        length = l;
+    }
+    public void setwidth (double w)
+    {
+        width = w;
+    }
+}
