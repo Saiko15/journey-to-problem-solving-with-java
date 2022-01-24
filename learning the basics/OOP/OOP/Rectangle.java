026b9491145bb6f067094fd49633d00d2083a907
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-24 10:49:47 +0200

    First OOP Project 0.0.4

--- learning the basics/OOP/OOP/Rectangle.java
+++ learning the basics/OOP/OOP/Rectangle.java
@@ -1,8 +1,8 @@
 package OOP;
 
 public class Rectangle {
-    private double length;
-    public double width;
+    private double length; 
+    public double width; // should be private ,, but this only for practice 
 
     public void setlength (double l)
     {
