df486d703c2d630eb852639941fddb1647180b46
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-24 11:19:36 +0200

    car class practicing 0.0.1

--- learning the basics/OOP/OOP/CarObject.java
+++ learning the basics/OOP/OOP/CarObject.java
@@ -3,6 +3,7 @@ package OOP;
 public class CarObject {
     public static void main(String[] args) {
         Car bmw;
+        // Rectangle r3; // also can be applies here because it's the same package
         String maker;
         int year;
         
