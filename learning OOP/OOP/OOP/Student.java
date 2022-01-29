3635b45c7b7b9738c17d9ec367a3c22e538b9c46
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-29 16:50:22 +0200

    final modifier

--- learning OOP/OOP/OOP/Student.java
+++ learning OOP/OOP/OOP/Student.java
@@ -2,4 +2,5 @@ package OOP;
 
 public class Student extends Person {  
     // use @override before overriding method from sub to super
+    // but final before method name prevent any other method to override it
 }
