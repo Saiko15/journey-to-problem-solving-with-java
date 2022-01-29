996e489e1a821578b2637ee82bba1214c12328d3
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-29 16:56:51 +0200

    Protected

--- learning OOP/OOP/OOP/Student.java
+++ learning OOP/OOP/OOP/Student.java
@@ -3,4 +3,5 @@ package OOP;
 public class Student extends Person {  
     // use @override before overriding method from sub to super
     // but final before method name prevent any other method to override it
+    // Protected instead of public, able to acces by subclasses but not out of class (between private and public)
 }
