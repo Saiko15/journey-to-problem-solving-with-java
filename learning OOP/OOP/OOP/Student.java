d5e7da4c11905ad20d973fd4225c0bd52ede56cd
Saiko15 <ahmedsaiko42@gmail.com>
2022-01-29 18:51:28 +0200

    Abstract

--- learning OOP/OOP/OOP/Student.java
+++ learning OOP/OOP/OOP/Student.java
@@ -4,4 +4,5 @@ public class Student extends Person {
     // use @override before overriding method from sub to super
     // but final before method name prevent any other method to override it
     // Protected instead of public, able to acces by subclasses but not out of class (between private and public)
+    // Abstract Class expect to be overriden and you can't take object from it.
 }
